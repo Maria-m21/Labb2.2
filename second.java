a01fdb256c00331c69aecb6f94a6c1f0320d8664
oltsaaa <a.oltsaa@gmail.com>
2024-03-05 21:19:11 +0200

    Initial commit

---
+++ second.java
@@ -0,0 +1,10 @@
+public class second {
+    public static void main(String[] args){
+        int number = 78;
+        int sum = (number/10) + (number%10);
+
+        System.out.println(+ sum);
+    }
+}
+
+
---
+++ third.java
@@ -0,0 +1,9 @@
+public class third {
+    public static void main(String[] args){
+        double a = 4.36748;
+        long rounderNumber = Math.round(a);
+
+        System.out.println(+ rounderNumber);
+    }
+}
+
